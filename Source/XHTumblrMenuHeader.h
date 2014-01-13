479ea0a0f5b5b5d95685845b6f782114c58e1bd9
Jack <xhzengAIB@gmail.com>
2014-01-13 20:50:38 +0000

    Design XHTumblrMenuItem model

---
+++ Source/XHTumblrMenuHeader.h
@@ -0,0 +1,17 @@
+//
+//  XHTumblrMenuHeader.h
+//  XHTumblrMenu
+//
+//  Created by 曾 宪华 on 14-1-13.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//
+
+#ifndef XHTumblrMenu_XHTumblrMenuHeader_h
+#define XHTumblrMenu_XHTumblrMenuHeader_h
+
+@class XHTumblrMenu;
+@class XHTumblrMenuItem;
+
+typedef void (^XHTumblrMenuViewSelectedBlock)(XHTumblrMenu *tumblrMenu, XHTumblrMenuItem *tumblrMenuItem);
+
+#endif
---
+++ Source/XHTumblrMenuItem.h
@@ -0,0 +1,16 @@
+//
+//  XHTumblrMenuItem.h
+//  XHTumblrMenu
+//
+//  Created by 曾 宪华 on 14-1-13.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "XHTumblrMenuHeader.h"
+
+@interface XHTumblrMenuItem : NSObject
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, strong) UIImage *icon;
+@property (nonatomic, copy) XHTumblrMenuViewSelectedBlock tumblrMenuViewSelectedBlock;
+@end
